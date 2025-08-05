3c2184559cdbe91b35f7d2b9b77e6cbf2d6e4d67
yaiv <yaiv.gm@gmail.com>
2025-08-04 21:14:05 -0600

    Vista administracion

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -6,7 +6,11 @@ use MVC\Router;
 
 class AdminController {
     public static function index ( Router $router ){
-        session_start();
+        //session_start();
+
+        //Consultar la BD 
+        
+
         $router->render('admin/index', [
             'nombre' => $_SESSION['nombre']
 
--- views/admin/index.php
+++ views/admin/index.php
@@ -1,4 +1,4 @@
-<h1 class="nombre-pagina">Panel de Administtración</h1>
+<h1 class="nombre-pagina">Panel de Administración</h1>
 
     <?php 
         include_once __DIR__ . '/../templates/barra.php' 
